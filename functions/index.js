36577b8dbeb3816211cabc93369008216728beef
nleavi11 <noahjlvtt@gmail.com>
2019-06-09 21:27:57 -0600

    questions pull in from data base and comments correctly upload; need to implement displaying comments and then deleting questions at the minimum

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,69 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+const app = express();
+
+var db = firebase.firestore();
+var questionsRef = db.collection('questions');
+
+app.get('/api/questions', async(req, res) => {
+    let querySnapshot = await questionsRef.get();
+    res.send(querySnapshot.docs.map(doc => doc.data()));
+})
+
+app.post('/api/questions', async (req, res) => {
+    let querySnapshot = await questionsRef.get();
+    let numRecords = querySnapshot.docs.length;
+
+    console.log(req.body.question);
+    console.log(req.body.id);
+    let newQuestion = {
+        id: req.body.id,
+        question: req.body.question,
+        comments: '',
+      };
+
+    questionsRef.doc(newQuestion.id.toString()).set(newQuestion);
+    res.send(newQuestion);
+})
+
+app.put('/api/questions/:id', async (req, res) => {
+  let id = req.params.id.toString();
+  console.log(id);
+  console.log(req.body.author);
+  console.log(req.body.text);
+  try{
+    questionsRef.doc(id).update({
+      comments: firebase.firestore.FieldValue.arrayUnion({author: req.body.author.toString(), text: req.body.text.toString()})
+    });
+    console.log("Document successfully updated");
+
+  } catch {
+    res.status(500).send("Error editing document");
+  }
+})
+
+// app.delete('/api/tickets/:id', async (req,res) => {
+//     let id = req.params.id.toString();
+//     var documentToDelete = ticketsRef.doc(id);
+//     try{
+//         var doc = await documentToDelete.get();
+//         if(!doc.exists){
+//             res.status(404).send("Sorry, that ticket doesn't exist");
+//             return;
+//         }
+//         else{
+//             documentToDelete.delete();
+//             res.sendStatus(200);
+//             return;
+//         }
+//     }catch(err){
+//         res.status(500).send("Error deleting document: ",err);
+//     }
 // });
+
+exports.app = functions.https.onRequest(app);
--- public/script.js
+++ public/script.js
@@ -1,36 +1,85 @@
 let app = new Vue({
   el: '#app',
   data: {
-    nextQuestionNumber: '1',
-    selectedQuestionNumber: '',
+    nextQuestionNumber: '',
+    selectedQuestionNumber: 2,
     addedQuestion: '',
     addedName: '',
     addedComment: '',
     comments: {    },
-    questions: {
-      // 1 : "my question",
-    },
+    questions: [],
     loading: false,
   },
+  created() {
+    this.getQuestions();
+  },
   methods: {
-    addComment() {
-      if (!(this.selectedQuestionNumber in this.comments))
-        Vue.set(app.comments, this.selectedQuestionNumber, new Array);
-      this.comments[this.selectedQuestionNumber].push({
-        author: this.addedName,
-        text: this.addedComment
-        //time: moment().format('LLLL')
-      });
-      this.addedName = '';
-      this.addedComment = '';
+    async getQuestions() {
+      try {
+        let response = await axios.get("/api/questions");
+        this.questions = response.data;
+        // console.log(this.questions.length);
+        this.nextQuestionNumber = this.questions.length + 1;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
     },
-    addQuestion() {
-      Vue.set(app.questions, this.nextQuestionNumber, { id: this.nextQuestionNumber , question: this.addedQuestion} );
-      this.nextQuestionNumber = parseInt(this.nextQuestionNumber) + 1;
-      this.addedQuestion = '';
+    async addQuestion() {
+      try {
+          let response = await axios.post("/api/questions", {
+            id: this.nextQuestionNumber,
+            question: this.addedQuestion
+          });
+          // console.log("ADD Question - RESPONSE: ",response);
+          this.addedQuestion = "";
+          this.nextQuestionNumber = parseInt(this.nextQuestionNumber) + 1;
+          // this.getTickets();
+          return true;
+        } catch (error) {
+          console.log(error);
+        }
     },
-    select(id) {
-      this.selectedQuestionNumber = id;
+    async addComment() {
+      try {
+        // console.log("i am at this point");
+        let response = await axios.put("/api/questions/" + this.selectedQuestionNumber, {
+            author: this.addedName,
+            text: this.addedComment
+            //time: moment().format('LLLL')
+          });
+          this.addedName = '';
+          this.addedComment = '';
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    // addComment() {
+    //   if (!(this.selectedQuestionNumber in this.comments))
+    //     Vue.set(app.comments, this.selectedQuestionNumber, new Array);
+    //   this.comments[this.selectedQuestionNumber].push({
+    //     author: this.addedName,
+    //     text: this.addedComment
+    //     //time: moment().format('LLLL')
+    //   });
+    //   this.addedName = '';
+    //   this.addedComment = '';
+    // },
+    // addQuestion() {
+    //   Vue.set(app.questions, this.nextQuestionNumber, { id: this.nextQuestionNumber , question: this.addedQuestion} );
+    //   this.nextQuestionNumber = parseInt(this.nextQuestionNumber) + 1;
+    //   this.addedQuestion = '';
+    // },
+    select(question) {
+      console.log(question.id);
+      this.selectedQuestionNumber = question.id;
+    }
+  },
+  watch: {
+    nextQuestionNumber(value, oldvalue) {
+      if(oldvalue !== '') {
+        this.getQuestions();
+      }
     }
   }
 })
